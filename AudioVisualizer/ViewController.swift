8f4e73c0d013f5c1d8933be454744cdf94b0577d
Ameed Sayeh <ameed.iyad.sayeh@gmail.com>
2021-03-05 20:05:56 +0200

    Initial Commit

---
+++ AudioVisualizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AudioVisualizer
+//
+//  Created by Ameed Sayeh on 05/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
